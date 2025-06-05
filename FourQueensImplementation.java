282fae6cadc82f54ce3046d4b80013b7dfdc5c4f
Jason Rudinsky <jrudy710@gmail.com>
2025-06-04 20:47:35 -0400

    Skeleton program for the eventual java program

---
+++ FourQueensImplementation.java
@@ -0,0 +1,23 @@
+/* Jason Rudinsky
+* June 4, 2025
+* Four Queens
+
+* This is the program that will attempt to solve the problem of where to place queens on an n by n board, 
+* so that none of the queens have the possibility of colliding with each other. 
+* This will be attempted by using a DFS approach, with the stack implementation that will be used in the form of an ArrayList.
+* Once the queens that are on the board are equal to the size of the chessboard, given that it will be a square board, 
+* the values will be returned to the user.
+
+* 6/4/2025 - Original Version
+*/
+
+import java.util.ArrayList;                                                                                 // Imports the arrayList libraries
+    
+public class FourQueensImplementation{                                                                      // Class Block
+    public static void main(String[] args){                                                                 // Main Method
+        
+    }
+
+
+
+}
